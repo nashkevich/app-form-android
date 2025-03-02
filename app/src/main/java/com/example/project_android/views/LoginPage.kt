22c83febf809d7388710710259074914c78281cd
nashkevich <agz-2001@mail.ru>
2025-03-02 13:56:59 +0100

    loginn page

--- app/src/main/java/com/example/project_android/views/LoginPage.kt
+++ app/src/main/java/com/example/project_android/views/LoginPage.kt
@@ -1,6 +1,7 @@
 package com.example.project_android.views
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,11 +10,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -21,6 +27,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.LinkAnnotation
@@ -29,8 +36,10 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.project_android.ui.theme.BorderColorInput
 import com.example.project_android.ui.theme.LinkColor
 import com.example.project_android.ui.theme.SupportTextColor
+import com.example.project_android.ui.theme.WhiteBg
 import com.example.project_android.utils.Routes
 
 @Composable
@@ -38,21 +47,24 @@ fun LoginPage(navController: NavController){
     var password = rememberSaveable { mutableStateOf("") }
     var username = rememberSaveable { mutableStateOf("") }
     Column (
-        Modifier.fillMaxSize(),
+
+        Modifier.fillMaxSize().padding(24.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.SpaceBetween
     ){
         Box(modifier = Modifier.size(50.dp).background(Color.Red))
         Text(text = "Sign In", style = TextStyle(
             color = LinkColor
         )
         )
-        TextField(
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
             value = username.value,
             onValueChange = {username.value = it},
-            label = { Text("Enter username") }
+            label = { Text("Enter user name") }
         )
         TextField(
+            modifier = Modifier.fillMaxWidth(),
             value = password.value,
             onValueChange = {password.value = it},
             label = { Text("Enter password") },
@@ -66,7 +78,7 @@ fun LoginPage(navController: NavController){
         )
         Button(onClick = {
             navController.navigate(Routes.registerPage)
-        }) {
+        }, modifier = Modifier.fillMaxWidth().height(50.dp)) {
             Text(text = "Sign In")
         }
         Text(text = "Or Sign In", style = TextStyle(
